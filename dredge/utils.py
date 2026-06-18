dc3f451187158f02507f90268c632093ac5852b0
Joe Zuntz <joezuntz@googlemail.com>
2026-06-18 14:48:10 +0100

    Fix haversine distance

--- dredge/utils.py
+++ dredge/utils.py
@@ -6,10 +6,16 @@ import numpy as np
 def haversine_distance(theta1, phi1, theta2, phi2):
     """
     Compute the haversine distance between two points on the sphere.
+    Careful!
+
+    First, these are healpix coordinates so the theta is the co-latitude
+    not the latitude
+
+    Second, many source swap theta and phi around
     """
     dlon = phi2 - phi1
-    dlat = theta2 - theta1
-    a = np.sin(dlat/2)**2 + np.cos(theta1) * np.cos(theta2) * np.sin(dlon/2)**2
+    dlat = theta1 - theta2 #switched because this is co-lat, but doesn't matter as the sin gets squared
+    a = np.sin(dlat/2)**2 + np.sin(theta1) * np.sin(theta2) * np.sin(dlon/2)**2
     c = 2 * np.arcsin(np.sqrt(a))
     return c
 
